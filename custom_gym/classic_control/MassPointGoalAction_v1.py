3cbba220dd0b63bbb45e30e6df34920159ff11b6
y4cj4sul3 <hl6540@gmail.com>
2018-10-17 02:10:46 +0800

    change action direction

--- custom_gym/classic_control/MassPointGoalAction_v1.py
+++ custom_gym/classic_control/MassPointGoalAction_v1.py
@@ -6,7 +6,7 @@ class MassPointGoalActionEnv_v1(MassPointGoalEnv_v1):
     def step(self, action):
         # Change Action
         action = np.clip(action, self.low_action, self.high_action)
-        action = action * 0.5
+        action = action * -1
 
         return super().step(action)
 
--- custom_gym/classic_control/MassPointTrajAction_v1.py
+++ custom_gym/classic_control/MassPointTrajAction_v1.py
@@ -6,7 +6,7 @@ class MassPointTrajActionEnv_v1(MassPointTrajEnv_v1):
     def step(self, action):
         # Change Action
         action = np.clip(action, self.low_action, self.high_action)
-        action = action * 0.5
+        action = action * -1
 
         return super().step(action)
 
