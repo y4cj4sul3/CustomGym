de10af6f1604e4940b15caa7060af94e71ac032f
recharrs <sliz97028@gmail.com>
2018-10-22 00:54:03 +0800

    clean up

--- rule_base/mass_point_traj_rule_base_v1.py
+++ rule_base/mass_point_traj_rule_base_v1.py
@@ -27,11 +27,13 @@ change_action = args.change_action
 # Create Environment
 env = gym.make(env_id)
 if is_record:
-		env = RecorderWrapper(env, file_path, file_format=file_format, save_on_finish=save_on_finish)
+    env = RecorderWrapper(
+        env, file_path, file_format=file_format, save_on_finish=save_on_finish)
 
 # Print action & observation space
 print(env.action_space)
 print(env.observation_space)
+abc = 1
 
 # Target Position
 targets = range(18, 180, 36)
@@ -48,92 +50,92 @@ threshold = rotate_scale * 0.01
 
 # Test Environment
 for i_episode in range(2500):
-	episode = 0
-	while episode < episode_per_task:
-		# Reset Environment
-		obs = env.reset()
-		
-		# specify target
-		task = np.array([i_episode % 2, 2+i_episode % 5])
+    episode = 0
+    while episode < episode_per_task:
+        # Reset Environment
+        obs = env.reset()
 
-		if not is_record:
-			# w/o recorder wrapper
-			obs = env.unwrapped.reset(task)
-		else:
-			# w/ recorder wrapper
-			obs = env.unwrapped.unwrapped.reset(task)
-			# hack expert action for bc
-			if is_for_bc:
-				env.traj['expert_action'] = []
+        # specify target
+        task = np.array([i_episode % 2, 2+i_episode % 5])
 
-		t = 0
-		rewards = 0
+        if not is_record:
+            # w/o recorder wrapper
+            obs = env.unwrapped.reset(task)
+        else:
+            # w/ recorder wrapper
+            obs = env.unwrapped.unwrapped.reset(task)
+            # hack expert action for bc
+            if is_for_bc:
+                env.traj['expert_action'] = []
 
-		agent = obs[:2]
-		instr = obs[-7:]
-		
-		#
-		face = np.array([0, 1])
-		# Run Episode
-		while True:
-			# Render Environment
-			# env.render()
-			# time.sleep(0.1)
-			# Interact with Environment
-			action = [0]
-			# target direction & delta angle
-			target_dir = targets[task[0]] - agent
-			
-			cos_theta = np.sum(target_dir * face) / (np.linalg.norm(target_dir)*np.linalg.norm(face))
-			cos_theta = np.clip(cos_theta, -1, 1)
-			delta_theta = np.arccos(cos_theta)
+        t = 0
+        rewards = 0
 
-			if delta_theta > threshold:
-				# right
-				dir_sign = 1
-				right_dir = np.array([target_dir[1], -target_dir[0]])
-				if np.sum(right_dir * face) < 0:
-					dir_sign = -1
+        agent = obs[:2]
+        instr = obs[-7:]
 
-				delta_theta = np.clip(delta_theta, -1, 1)
-				action[0] = dir_sign * delta_theta / rotate_scale
-				action[0] = np.clip(action[0], -1, 1)
+        #
+        face = np.array([0, 1])
+        # Run Episode
+        while True:
+            # Render Environment
+            # env.render()
+            # time.sleep(0.1)
+            # Interact with Environment
+            action = [0]
+            # target direction & delta angle
+            target_dir = targets[task[0]] - agent
 
-			if change_action: action[0] = action[0] * -1
-			expert_action = np.array(action)
+            cos_theta = np.sum(target_dir * face) / \
+                (np.linalg.norm(target_dir)*np.linalg.norm(face))
+            cos_theta = np.clip(cos_theta, -1, 1)
+            delta_theta = np.arccos(cos_theta)
 
-			# hack expert action for bc
-			if is_record and is_for_bc:
-				env.traj['expert_action'].append(expert_action)
+            if delta_theta > threshold:
+                # right
+                dir_sign = 1
+                right_dir = np.array([target_dir[1], -target_dir[0]])
+                if np.sum(right_dir * face) < 0:
+                    dir_sign = -1
 
-			# Random action
-			action = env.action_space.sample()
-			#if change_action: action = action * -1
-			action = action * noise_rate + expert_action * (1 - noise_rate)
+                delta_theta = np.clip(delta_theta, -1, 1)
+                action[0] = dir_sign * delta_theta / rotate_scale
+                action[0] = np.clip(action[0], -1, 1)
 
-			obs, reward, done, info = env.step(action)
+            if change_action: action[0] = action[0] * -1
+            expert_action = np.array(action)
 
-			if info['done_status'] == 'Finish Task':
-				print(rewards)
-			rewards += reward
-			
-			if info['done_status'] == 'Right Target':
-				print(rewards)
-				task = task[1:]
-				
+            # hack expert action for bc
+            if is_record and is_for_bc:
+                env.traj['expert_action'].append(expert_action)
 
-			agent = obs[:2]
-			face = obs[2:4]
+            # Random action
+            action = env.action_space.sample()
+            if change_action: action = action * -1
+            action = action * noise_rate + expert_action * (1 - noise_rate)
 
-			t = t+1
+            obs, reward, done, info = env.step(action)
 
-			# Check Done
-			if done:
-				print('Reward: {}'.format(rewards))
-				print("Episode finished after {} timesteps".format(t+1))
-				if (not save_on_finish) or info['done_status'] == 'Finish Task': 
-					episode += 1
-				break
+            if info['done_status'] == 'Finish Task':
+                print(rewards)
+            rewards += reward
+
+            if info['done_status'] == 'Right Target':
+                print(rewards)
+                task = task[1:]
+
+            agent = obs[:2]
+            face = obs[2:4]
+
+            t = t+1
+
+            # Check Done
+            if done:
+                print('Reward: {}'.format(rewards))
+                print("Episode finished after {} timesteps".format(t+1))
+                if (not save_on_finish) or info['done_status'] == 'Finish Task':
+                    episode += 1
+                break
 
 # Close Environment
 env.close()
